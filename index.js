55d17e058c2b11202f3f0992507c2be5f0168e98
Moshiur Rahman <rifatmoshiur00@gmail.com>
2024-05-08 01:30:57 +0600

    post method to add product, read 3 different types of data by name and also id dynamically functionality completed. for add to cart, a new database collection created and delete method implement successfully.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mi4xixr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,22 +28,96 @@ async function run() {
     await client.connect();
 
     const productsCollection = client.db('Fashion_collections').collection('products')
+    const myCart = client.db('Fashion_collections').collection('cart')
 
     // Read OPeration
 
-    app.get('/products',async(req,res)=>{
-        const result = await productsCollection.find().toArray()
-        res.send(result)
+    app.get('/products', async (req, res) => {
+      const result = await productsCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/products/:name', async (req, res) => {
+      const name = req.params.name
+      const query = { brand: name }
+      const result = await productsCollection.find(query).toArray()
+      res.send(result)
+    })
+    // _____Update route______
+
+    app.get('/update/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+    })
+
+    //----Details-------
+    app.get('/details/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.findOne(query)
+      res.send(result)
     })
 
     // POST operation
-    app.post('/products',async(req,res)=>{
-        const productDetails = req.body
-        const result = await productsCollection.insertOne(productDetails)
-        res.send(result)
+    app.post('/products', async (req, res) => {
+      const productDetails = req.body
+      const result = await productsCollection.insertOne(productDetails)
+      res.send(result)
     })
 
-    
+    // PUT or UPDATE operations
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedUSer = {
+        $set: {
+         name : data.name,
+         price : data.price,
+         rating : data.rating,
+         description : data.description,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updatedUSer,
+        options
+      );
+      res.send(result);
+    })
+
+
+    // -------------------My cart----------------
+
+    app.post('/carts',async(req,res)=>{
+      const details = req.body
+      const result = await myCart.insertOne(details)
+      res.send(result)
+    })
+
+    app.get('/carts', async (req, res) => {
+      const result = await myCart.find().toArray()
+      res.send(result)
+    })
+    app.get('/carts/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await myCart.findOne(query)
+      res.send(result)
+    })
+
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await myCart.deleteOne(query)
+      res.send(result)
+    })
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -58,11 +132,11 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('VogueVault web server is running like neuron!!')
+app.get('/', (req, res) => {
+  res.send('VogueVault web server is running like neuron!!')
 })
 
 
-app.listen(port,()=>{
-    console.log(`My port is running on: ${port}`);
+app.listen(port, () => {
+  console.log(`My port is running on: ${port}`);
 })
